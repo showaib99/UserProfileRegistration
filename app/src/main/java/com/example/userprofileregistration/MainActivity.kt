f0a6dcdc640c2985e7e54fb263334ac960c43d36
Showaib Alam <showaib99@gmail.com>
2024-09-23 13:14:45 +0600

    First Commit

---
+++ app/src/main/java/com/example/userprofileregistration/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.userprofileregistration
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var listButton:Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
